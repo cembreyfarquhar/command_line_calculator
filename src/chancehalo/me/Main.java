af0423eab0ce4ab4a45d46815e7e40d1585f39b8
Chance Embrey-Farquhar <cembreyfarquhar@gmail.com>
2019-08-06 11:10:28 -0500

    first commit

---
+++ src/chancehalo/me/Main.java
@@ -0,0 +1,15 @@
+package chancehalo.me;
+
+public class Main {
+
+    public static void main(String[] args) {
+        int result = add(5, 2);
+        System.out.println(result);
+    }
+
+    public static int add(int x, int y) {
+        return x + y;
+    }
+
+    public static int subtract
+}
